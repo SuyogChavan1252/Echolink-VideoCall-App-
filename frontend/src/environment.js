de13790312b9ac45a71b9a35c8326065f132dbdf
SuyogChavan1252 <144939782+SuyogChavan1252@users.noreply.github.com>
2025-04-20 14:01:59 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,5 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://echolink-videocall-app.onrender.com" :
-
-    "http://localhost:8000"
+const server = "https://echolink-videocall-app.onrender.com" ;
 
 
 export default server;
